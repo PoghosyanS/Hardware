794f6501cb27e5460789ac7e69678058ab3be2f3
Poghosyan <poghosyan2014@bk.ru>
2023-06-07 12:47:31 +0400

    homework

--- proj/tled.v
+++ proj/tled.v
@@ -1,39 +1,33 @@
 module pwm_control_tb;
 
-  reg clk;
-  wire led;
+  	reg clk;
+  	wire led;
 
-  // Instantiate the PWM module
-  pwm_control dut (
-    .clk(clk),
-    .led(led)
+  	pwm_control dut (
+    	.clk(clk),
+    	.led(led)
   );
 
-  // Clock generator
-  always begin
-    #5 clk = ~clk;
-  end
+  	always begin
+    	#5 clk = ~clk;
+  	end
 
-  // Testbench stimulus
-  initial begin
-    clk = 0;
-    #10;
+  	initial begin
+    	clk = 0;
+    	#10;
 
-    // Gradually increase brightness
-    #100;
-    $display("Increasing brightness...");
-    repeat(255) begin
-      #10;
-    end
+    	#100;
+    	$display("Increasing brightness...");
+    	repeat(255) begin
+      	#10;
+    	end
 
-    // Gradually decrease brightness
-    #100;
-    $display("Decreasing brightness...");
-    repeat(255) begin
-      #10;
-    end
+    	#100;
+    	$display("Decreasing brightness...");
+    	repeat(255) begin
+      	#10;
+    	end
 
-    // End simulation
     $finish;
   end
 
